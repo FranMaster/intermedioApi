97d541fd3e4358f6ca6c371a317e6dc21f42a0bc
Francisco <Francisco.jimenez.cohen@gmail.com>
2020-06-05 15:35:52 -0300

    Creacion de Servidor con Express

---
+++ Server/main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+let app = express();
+let puerto = 3000;
+
+const axios = require('axios');
+
+const getpaises = async() => {
+
+    const instance = axios.create({
+        baseURL: `https://restcountries.eu/rest/v2/all`,
+        headers: {}
+    });
+    const resp = await instance.get();
+    return resp
+}
+
+// getpaises()
+//     .then(console.log)
+//     .catch(console.log);
+
+
+app.listen(puerto, () => {
+    console.log(`Escuchando por el puerto: ${puerto}`);
+})
